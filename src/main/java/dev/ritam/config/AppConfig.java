5d4623e2615e24f4a98306ed4939e9cbac7b7238
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-11-30 12:44:57 +0530

    Write Test

--- src/main/java/dev/ritam/config/AppConfig.java
+++ src/main/java/dev/ritam/config/AppConfig.java
@@ -1,7 +1,12 @@
 package dev.ritam.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@Configuration
 @EnableAspectJAutoProxy
+@ComponentScan("dev.ritam")
 public class AppConfig {
+
 }
--- src/test/java/dev/ritam/AppTest.java
+++ src/test/java/dev/ritam/AppTest.java
@@ -1,18 +1,23 @@
 package dev.ritam;
 
+import dev.ritam.config.AppConfig;
+import dev.ritam.model.Bank;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+    private static final ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void test() {
+        Bank bank = context.getBean(Bank.class);
+
+        assertNotNull(bank);
+        assertEquals(10000, bank.getBalance());
+        assertEquals(6789, bank.getPinCode());
     }
 }
