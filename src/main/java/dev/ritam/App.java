c14c1970aaf014c53de45d0fa35bbf94dacc2417
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-11-30 16:49:40 +0530

    Add try-catch in UI to ignore the RuntimeException

--- src/main/java/dev/ritam/App.java
+++ src/main/java/dev/ritam/App.java
@@ -13,49 +13,52 @@ public class App {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         Bank bank = context.getBean(Bank.class);
 
-        while (true) {
-            System.out.println("Enter your pin:");
-            int tempPin = scanner.nextInt();
-            bank.setTempPin(tempPin);
+        try {
+            while (true) {
+                System.out.println("Enter your pin:");
+                int tempPin = scanner.nextInt();
+                bank.setTempPin(tempPin);
 
-            System.out.println("1. Show Balance");
-            System.out.println("2. Cash Withdraw");
-            System.out.println("3. Cash Deposit");
-            System.out.println("4. Change Pin");
-            System.out.println("5. Exit");
-            System.out.println("Enter your choice: ");
-            int choice = scanner.nextInt();
+                System.out.println("1. Show Balance");
+                System.out.println("2. Cash Withdraw");
+                System.out.println("3. Cash Deposit");
+                System.out.println("4. Change Pin");
+                System.out.println("5. Exit");
+                System.out.println("Enter your choice: ");
+                int choice = scanner.nextInt();
 
-            switch (choice) {
-                case 1: {
-                    bank.showBalance();
-                    break;
-                }
-                case 2: {
-                    System.out.println("Enter amount to be withdrawn:");
-                    int amount = scanner.nextInt();
-                    bank.withdraw(amount);
-                    break;
-                }
-                case 3: {
-                    System.out.println("Enter the amount to be deposited:");
-                    int amount = scanner.nextInt();
-                    bank.deposit(amount);
-                    break;
-                }
-                case 4: {
-                    System.out.println("Enter new pin:");
-                    int newPin = scanner.nextInt();
-                    bank.changePin(newPin);
-                    break;
-                }
-                case 5: {
-                    System.exit(0);
-                }
-                default: {
-                    System.err.println("UNKNOWN INPUT!");
+                switch (choice) {
+                    case 1: {
+                        bank.showBalance();
+                        break;
+                    }
+                    case 2: {
+                        System.out.println("Enter amount to be withdrawn:");
+                        int amount = scanner.nextInt();
+                        bank.withdraw(amount);
+                        break;
+                    }
+                    case 3: {
+                        System.out.println("Enter the amount to be deposited:");
+                        int amount = scanner.nextInt();
+                        bank.deposit(amount);
+                        break;
+                    }
+                    case 4: {
+                        System.out.println("Enter new pin:");
+                        int newPin = scanner.nextInt();
+                        bank.changePin(newPin);
+                        break;
+                    }
+                    case 5: {
+                        System.exit(0);
+                    }
+                    default: {
+                        System.err.println("UNKNOWN INPUT!");
+                    }
                 }
             }
+        } catch (Exception ignore) {
         }
     }
 }
